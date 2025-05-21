ed2a38433c5e39c4ee6ff182ecf0f2e1a9f70b8b
Alexander Voronkov <pubgplayer29112004@gmail.com>
2025-05-22 01:19:25 +0200

    fix compilation

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,20 +1,30 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-
 import dotenv from "dotenv";
 dotenv.config();
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const PROVIDER_URL = process.env.PROVIDER_URL;
+const ETHERSCAN_KEY = process.env.ETHERSCAN_KEY;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
   networks: {
-    sepolia: {
-      url: `${process.env.PROVIDER_URL}`,
-      accounts: [`${process.env.PRIVATE_KEY}`],
+    localhost: {
+      url: "http://127.0.0.1:8545",
     },
+    ...(PRIVATE_KEY && PROVIDER_URL
+      ? {
+          sepolia: {
+            url: PROVIDER_URL,
+            accounts: [PRIVATE_KEY],
+          },
+        }
+      : {}),
   },
   etherscan: {
     apiKey: {
-      sepolia: `${process.env.ETHERSCAN_KEY}`,
+      sepolia: ETHERSCAN_KEY || "",
     },
   },
 };
