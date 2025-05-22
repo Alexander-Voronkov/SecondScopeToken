d677a246695d397e591ee971fa6ea6bf4cece329
Alexander Voronkov <pubgplayer29112004@gmail.com>
2025-05-22 14:31:25 +0200

    test husky

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -1,50 +1,59 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.28;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+
 // import {ERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 
+/// @title My erc20 realization
+/// @author Oleksandr Voronkov
+/// @notice Use this contract to create erc-20 based tokens
+/// @dev dev
 contract ERC20 is IERC20 {
-    uint256 internal _totalSupply;
-    mapping(address => uint256) internal _balances;
-    mapping(address => mapping(address => uint256)) internal _allowancesOwnerSpender;
-    
-    function totalSupply() public view override returns(uint256) {
-        return _totalSupply;
-    }
+  uint256 internal _totalSupply;
+  mapping(address => uint256) internal _balances;
+  mapping(address => mapping(address => uint256)) internal _allowancesOwnerSpender;
 
-    function balanceOf(address account) public view override returns (uint256) {
-        return _balances[account];
-    }
+  function totalSupply() public view override returns (uint256) {
+    return _totalSupply;
+  }
 
-    function transfer(address to, uint256 amount) public virtual override returns (bool) {
-        require(_balances[msg.sender] >= amount, "Insufficient balance");
-        _balances[msg.sender]-= amount;
-        _balances[to]+= amount;
-        emit Transfer(msg.sender, to, amount);
-        return true;
-    }
+  function balanceOf(address account) public view override returns (uint256) {
+    return _balances[account];
+  }
 
-    function allowance(address owner, address spender) public view override returns (uint256) {
-        return _allowancesOwnerSpender[owner][spender];
-    }
+  function transfer(address to, uint256 amount) public virtual override returns (bool) {
+    require(_balances[msg.sender] >= amount, 'Insufficient balance');
+    _balances[msg.sender] -= amount;
+    _balances[to] += amount;
+    emit Transfer(msg.sender, to, amount);
+    return true;
+  }
 
-    function approve(address spender, uint256 amount) public override returns (bool) {
-        _allowancesOwnerSpender[msg.sender][spender] = amount;
-        emit Approval(msg.sender, spender, amount);
-        return true;
-    }
+  function allowance(address owner, address spender) public view override returns (uint256) {
+    return _allowancesOwnerSpender[owner][spender];
+  }
 
-    function transferFrom(address from, address to, uint256 amount) public virtual override returns (bool) {
-        require(_balances[from] >= amount, "Insufficient balance.");
-        require(_allowancesOwnerSpender[from][msg.sender] >= amount, "Not allowed to spend this much.");
-        
-        _balances[from] -= amount;
-        _balances[to] += amount;
-        _allowancesOwnerSpender[from][msg.sender] -= amount;
+  function approve(address spender, uint256 amount) public override returns (bool) {
+    _allowancesOwnerSpender[msg.sender][spender] = amount;
+    emit Approval(msg.sender, spender, amount);
+    return true;
+  }
 
-        emit Transfer(from, to, amount);
+  function transferFrom(
+    address from,
+    address to,
+    uint256 amount
+  ) public virtual override returns (bool) {
+    require(_balances[from] >= amount, 'Insufficient balance.');
+    require(_allowancesOwnerSpender[from][msg.sender] >= amount, 'Not allowed to spend this much.');
 
-        return true;
-    }
+    _balances[from] -= amount;
+    _balances[to] += amount;
+    _allowancesOwnerSpender[from][msg.sender] -= amount;
+
+    emit Transfer(from, to, amount);
+
+    return true;
+  }
 }
